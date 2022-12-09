54579521aa7da7e84a2a8fdfc3aa2ceed98d04ab
GRandom <abupilha@gmail.com>
2022-12-09 14:33:53 -0300

    Atualizacao na index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GetawyIntentBits, GatewayIntentBits } = require('discord.js')
+const { Client, Collection, GatewayIntentBits } = require('discord.js')
 const config = require(`./config.json`)
 const client = new Client ({ 
     intents: [
@@ -12,18 +12,28 @@ const client = new Client ({
         status: 'idle'
     } 
 })
-
+const hand = require(`./handling`)(client, Collection, GatewayIntentBits)
 
 client.on(`ready`, () => {
     console.log(`Log in ${client.user.tag}`)
 })
 
 client.on(`interactionCreate`, async interaction => {
-    if(!interaction.isChatInputCommand()) return
-
-    if(interaction.commandName === `ping`){
-        await interaction.reply(`Pong!`)
+    if (!interaction.isChatInputCommand()) return;
+  
+    const command = interaction.client.commands.get(interaction.commandName);
+  
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
     }
-})
+  
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  });
 
 client.login(config.token)
