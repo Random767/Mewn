f04e28732f222ee9a1f6d05a1611dce51cf1ca98
GRandom <abupilha@gmail.com>
2022-12-10 19:47:06 -0300

    update ping

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,8 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Responde com pong'),
-	async execute(interaction) {
-		await interaction.reply('Ponguei!');
+	async execute(interaction, client) {
+		const m = await interaction.reply({ content: 'Estou pongando...', fetchReply: true })
+		await interaction.editReply(`📡 | Ponguei! Meu ping: ${client.ws.ping}ms\n 🏃🏼 | Latencia msg: ${m.createdTimestamp - interaction.createdTimestamp}ms\n 🖥 | Ping API: ${Date.now() - interaction.createdTimestamp}`);
 	},
 };
