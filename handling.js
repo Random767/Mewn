7b64b50b8eabd9e519473c5fa29238e464bee7ee
GRandom <abupilha@gmail.com>
2022-12-10 19:43:01 -0300

    Update handler

--- handling.js
+++ handling.js
@@ -1,22 +1,31 @@
 const { REST, Routes, Collection } = require('discord.js');
 const fs = require(`fs`)
 const path = require('node:path');
+const config = require('./config.json')
 
-module.exports = (client) => {
-  client.commands = new Collection();
-  const commandsPath = path.join(__dirname + `/commands/`)
-  const commandFIles = fs.readdirSync(commandsPath).filter(file => file.endsWith(`.js`))
+module.exports = (client, rest) => {
+  client.commands = new Collection()
+  const commands = [];
+  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-  for(const file of commandFIles){
-    const filePath = path.join(commandsPath, file)
-    command = require(filePath)
-
-    if(`data` in command && `execute` in command) {
-      client.commands.set(command.data.name, command)
-      console.log(`[${command.data.name}] Carregado`)
-    } else {
-      console.log(`[Aviso]: O comando em ${filePath} nao tem a propriedade "data" ou "execute"`)
-    }
+  for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command)
   }
 
+  (async () => {
+    try {
+      console.log(`[Reload] recarregando ${commands.length} comandos`);
+
+      const data = await rest.put(
+        Routes.applicationCommands('1049428107150512148'),
+        { body: commands },
+      )
+
+      console.log('[Load] Comandos recarregados ' + data.length)
+    } catch(err){
+      console.error(err);
+    }
+  })();
 }
