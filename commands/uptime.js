ab6f96164fce0fad13a27a87d3d0b1eb3f964cab
GRandom <abupilha@gmail.com>
2022-12-10 19:59:01 -0300

    Add uptime

---
+++ commands/uptime.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime')
+        .setDescription('Veja a quanto tempo o bot está ligado'),
+    async execute(interaction, client){
+        let dias = 0
+        let semanas = 0
+
+        let uptime = ''
+        let totalSegundos = (client.uptime / 1000)
+        let horas = Math.floor(totalSegundos / 3600)
+        totalSegundos %= 3600
+        let minutos = Math.floor(totalSegundos / 60)
+        let segundos = Math.floor(totalSegundos % 60)
+
+        if(horas > 23){
+            dias = dias + 1
+            horas = 0
+        }
+        if(dias == 7){
+            dias = 0
+            semanas = semanas +1
+        }
+        if(minutos > 60){
+            minutos = 0
+        }
+
+        uptime += ` **${semanas} semanas** **${dias}d** **${horas}h** **${minutos}m** **${segundos}s**`
+        await interaction.reply(`Fazem exatamente ${uptime} que eu tô acordado, e contando...`)
+    }
+};
