144ed9da3b09f20ec7175e679c7e9cc3642e7ef3
GRandom <abupilha@gmail.com>
2023-01-21 12:46:48 -0300

    Melhorias no botinfo

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -6,10 +6,59 @@ module.exports = {
         .setName('botinfo')
         .setDescription('Veja as informações do bot'),
     async execute(interaction, client){
+        let ram = Math.round(process.memoryUsage().rss / 1024 / 1024).toFixed(0)
+        let cpu = Math.round(process.cpuUsage().system / 1024 / 1024).toString()
+        if(cpu < 1){
+            cpu = (process.cpuUsage().system / 1024 / 1024).toFixed(2)
+        }
+
         const botinfo = new EmbedBuilder()
             .setTitle('Minhas informações')
-            .setDescription('Clique [aqui](https://discord.com/oauth2/authorize?client_id=1049428107150512148&scope=bot&permissions=8) para me adicionar')
-            .addFields({ name: "Desenvolvedores", value: `${client.users.cache.get('633764019559202836').tag} \nintratec#2443 \npolishnelo#4410`, inline: true})
+            .setDescription('Clique [aqui](https://discord.com/oauth2/authorize?client_id=1049428107150512148&scope=bot&permissions=8) para me adicionar\nMeu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)')
+            .addFields({ 
+                    name: "Desenvolvedores",
+                    value: `\`\`\`${client.users.cache.get('633764019559202836').tag}, intratec#2443, polishnelo#4410\`\`\``, 
+                    inline: false
+                })
+            
+            .addFields({ 
+                name: "Linguagem de programação", 
+                value: `\`\`\`NodeJs\`\`\``, 
+                inline: true
+            })
+            .addFields({ 
+                name: "Host", 
+                value: `\`\`\`Raspberry pi\`\`\``, 
+                inline: true})
+            .addFields({ 
+                name: "Ping", 
+                value: `\`\`\`${client.ws.ping}ms\`\`\``, 
+                inline: true})
+            
+            .addFields({ 
+                name: "Servidores", 
+                value: `\`\`\`${client.guilds.cache.size}\`\`\``, 
+                inline: true})
+            .addFields({ 
+                name: "Canais", 
+                value: `\`\`\`${client.channels.cache.size}\`\`\``, 
+                inline: true 
+            })
+            .addFields({ 
+                name: "Usuários", 
+                value: `\`\`\`${client.users.cache.size}\`\`\``, 
+                inline: true 
+            })
+            
+            .addFields({ 
+                name: "Utilização da ram", 
+                value: `\`\`\`${ram}MB\`\`\``, 
+                inline: true })
+            .addFields({ 
+                name: "Uso da CPU", 
+                value: `\`\`\`${cpu}%\`\`\``, 
+                inline: true })
+            
             .setThumbnail(client.user.displayAvatarURL({ dinamic: true, size: 4096, format: "png" }))
             .setColor('#2f3136')
         await interaction.reply({ embeds: [botinfo] })
