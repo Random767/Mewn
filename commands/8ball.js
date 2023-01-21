6ada576c88cc63efc82827819ba5c25c0532f890
GRandom <abupilha@gmail.com>
2023-01-21 07:44:26 -0300

    8ball adicionado

---
+++ commands/8ball.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName(`8ball`)
+        .setDescription(`Pergunte algo e eu responderei com sim ou não`)
+        .addStringOption(option =>
+            option.setName('frase')
+                .setDescription('Digite algo')
+                .setRequired(true)),
+    async execute(interaction) {
+        let results = [
+            'Sim',
+            'Não',
+            'Talvez :v',
+            'Boa pergunta',
+            'Não sei',
+            'Pergunte pra outra pessoa',
+            'Com certeza não',
+            'Com certeza',
+            'Sem dúvida',
+            'Provavelmente',
+            'Eu sou apenas um bot, nao posso responder essas perguntas',
+            'Sim, sem sombra de dúvida!',
+            "Não :D"
+        ]
+
+        let result = Math.floor((Math.random() * results.length));
+        await interaction.reply(results[result]);
+    },
+};
