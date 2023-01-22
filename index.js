dba21e81b1d065eedec572391d5543e24716044c
GRandom <abupilha@gmail.com>
2023-01-21 21:52:23 -0300

    Comando avatar adicionado

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ client.on('messageCreate', (message) => {
     const response = new EmbedBuilder()
       .setTitle('Central de ajuda')
       .setDescription(`Olá ${message.author.username}! Eu sou o ${client.user.username}, um bot em fase beta, mas futuramente vai vir cheio de functionalidades`)
-      .addFields(({ name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: https://discord.gg/3WYfg5RV9T', inline: true} ))
-      .addFields(({ name: '🖥 • Eu sou open source', value: 'Meu repositório no github: https://github.com/Random767/Mewn-Bot', inline: true} ))
+      .addFields(({ name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: [discord.gg/3WYfg5RV9T](https://discord.gg/3WYfg5RV9T)', inline: true} ))
+      .addFields(({ name: '🖥 • Eu sou open source', value: 'Meu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)', inline: true} ))
       
       .setThumbnail(client.user.displayAvatarURL({ dinamic: true, size: 4096, format: "png" }))
       .setColor('#2f3136')
