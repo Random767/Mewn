55c48f37d6129fbd9e6616c4cc53a6311bd378f7
GRandom <abupilha@gmail.com>
2023-01-22 23:53:29 -0300

    Correção de bugs no userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,4 +1,3 @@
-const Discord = require("discord.js");
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const moment = require('moment')
 moment.locale('pt-BR')
@@ -12,47 +11,83 @@ module.exports = {
                 .setDescription('Digite um id ou mencione um usuário')
                 .setRequired(false)),
     async execute(interaction, client) {
-        const getUser = interaction.options.getString('usuário') || interaction.user.id
-        const userinfo = client.users.cache.find(user => user.username.toLowerCase() === getUser.toLowerCase()) || client.users.cache.get(getUser)
+        const getUser = interaction.options.getString('usuário') || interaction.member.id
+        const userinfo =  client.users.cache.find(user => user.username.toLowerCase() === getUser.toLowerCase()) || client.users.cache.get(getUser)
+        const joinedAt = interaction.guild.members.cache.get(getUser).joinedTimestamp
 
-        console.log(client.members.cache.map(x => x))
         if(!userinfo){
             await interaction.reply('Desculpe, não consegui encontrar o usuário')
+            return
         }
 
         const uname = userinfo.username
         const utag = userinfo.tag
         const uid = userinfo.id
         const ucreatedat = `${moment(userinfo.createdAt).format('LLLL')} (${moment(userinfo.createdAt).fromNow()})`
-        const ujoined = `${moment(userinfo.joinedAt).format('LLLL')} (${moment(userinfo.joinedAt).fromNow()})`
+        const ujoined = `${moment(joinedAt).format('LLLL')} (${moment(joinedAt).fromNow()})`
         const uavatar = userinfo.displayAvatarURL({dynamic: true})
 
-        const embed = new EmbedBuilder()
-            .setTitle(uname)
-            .addFields({
-                name: "Tag",
-                value: utag,
-                inline: true
-            })
-            .addFields({
-                name: "Id",
-                value: uid,
-                inline: true
-            })
-            .addFields({
-                name: "Conta criada em",
-                value: ucreatedat,
-                inline: true
-            })
-            .addFields({
-                name: "Entrou aqui em",
-                value: ujoined,
-                inline: true
-            })
+        function isMember(){
+            let result = interaction.guild.members.cache.filter(a => a.id === userinfo.id).size
+            if(result === '1'){
+                return true
+            } else{
+                return false
+            }
+        }
+
+        if(isMember){
+            const embed = new EmbedBuilder()
+                .setTitle(uname)
+                .addFields({
+                    name: "Tag",
+                    value: utag,
+                    inline: true
+                })
+                .addFields({
+                    name: "Id",
+                    value: uid,
+                    inline: true
+                })
+                .addFields({
+                    name: "Conta criada em",
+                    value: ucreatedat,
+                    inline: true
+                })
+                .addFields({
+                    name: "Entrou aqui em",
+                    value: ujoined,
+                    inline: true
+                })
             .setThumbnail(uavatar)
-        
+            .setColor('#2f3136')
 
+            await interaction.reply({ embeds: [embed] });
+            return
+        } else{
+            const inServer = new EmbedBuilder()
+                .setTitle(uname)
+                .addFields({
+                    name: "Tag",
+                    value: utag,
+                    inline: true
+                })
+                .addFields({
+                    name: "Id",
+                    value: uid,
+                    inline: true
+                })
+                .addFields({
+                    name: "Conta criada em",
+                    value: ucreatedat,
+                    inline: true
+                })
+            .setThumbnail(uavatar)
+            .setColor('#2f3136')
 
-        await interaction.reply({ embeds: [embed] });
+        await interaction.reply({ embeds: [inServer] });
+        return
+
+        }
     },
 };
