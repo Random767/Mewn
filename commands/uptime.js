0e3f836e11376f06cf67c5d402a8590514aafbab
GRandom <abupilha@gmail.com>
2023-01-25 18:17:20 -0300

    corrigido o bug de horas do uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -6,27 +6,16 @@ module.exports = {
         .setName('uptime')
         .setDescription('Veja a quanto tempo o bot está ligado'),
     async execute(interaction, client){
-        let dias = 0
         let semanas = 0
 
         let uptime = ''
         let totalSegundos = (client.uptime / 1000)
+        let dias = Math.floor(totalSegundos / 86400);
+        totalSegundos %= 86400;
         let horas = Math.floor(totalSegundos / 3600)
         totalSegundos %= 3600
         let minutos = Math.floor(totalSegundos / 60)
         let segundos = Math.floor(totalSegundos % 60)
-
-        if(horas > 23){
-            dias = dias + 1
-            horas = 0
-        }
-        if(dias == 7){
-            dias = 0
-            semanas = semanas +1
-        }
-        if(minutos > 60){
-            minutos = 0
-        }
 
         uptime += ` **${semanas} semanas** **${dias}d** **${horas}h** **${minutos}m** **${segundos}s**`
         await interaction.reply(`Fazem exatamente ${uptime} que eu tô acordado, a vontade de mimir ta forte kkkk`)
