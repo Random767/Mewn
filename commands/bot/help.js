3ce61185fe2579e5a0cb1ea4bc43e3a2d0e03a6d
GRandom <abupilha@gmail.com>
2023-02-05 16:55:20 -0300

    command moved to commands command

---
+++ commands/bot/help.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Utilize esse comando para ajuda'),
+    async execute(interaction, client){
+        const response = new EmbedBuilder()
+            .setTitle('Central de ajuda')
+            .setDescription(`Olá ${interaction.user.username}! Eu sou o ${client.user.username}, um bot em fase beta, mas futuramente vai vir cheio de functionalidades`)
+            .addFields(({ name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: [discord.gg/3WYfg5RV9T](https://discord.gg/3WYfg5RV9T)', inline: true} ))
+            .addFields(({ name: '🖥 • Eu sou open source', value: 'Meu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)', inline: true} ))
+            .addFields({ name: "🤔 • Quer ver todos os meus comandos?", value: "Utilize o comando /commands", inline: false })
+        
+            .setThumbnail(client.user.displayAvatarURL({ dinamic: true, size: 4096, format: "png" }))
+            .setColor('#2f3136')
+        await interaction.reply({ embeds: [response] })
+    }
+}
