4c8cd6ab442011fdf139ea8f4b9e9c96c234455d
GRandom <abupilha@gmail.com>
2023-02-17 19:36:17 -0300

    bug fixing when quoting bot id

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,7 +6,7 @@ module.exports = {
     name: Events.MessageCreate,
     once: false,
     exec(message){
-        if(message.content.includes(client.user.id) && (!message.author.bot)){
+        if(message.content.includes(`<@${client.user.id}>`) && (!message.author.bot)){
             message.reply(`Olá ${message.author.username}! Meu nome é ${client.user.username}, utilize /help para mais informações`)
           }
     }
