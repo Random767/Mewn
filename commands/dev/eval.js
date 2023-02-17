5a32a9165a08f2fd85d36572978f605152d67b8f
GRandom <abupilha@gmail.com>
2023-02-17 20:26:21 -0300

    add eval command

---
+++ commands/dev/eval.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js')
+const { developerCommands } = require('./../../config.json')
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('eval')
+        .setDescription('Execute códigos no Mewn')
+        .setDMPermission(false)
+        .addStringOption(option => 
+            option.setName('eval')
+                .setDescription('Escreva um código')
+                .setRequired(true))
+                .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction){
+        const clean = text => {
+            if (typeof(text) === "string")
+              return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+            else
+                return text;
+          }
+        if(!developerCommands.whitelist.users.includes(interaction.user.id)) {
+            return interaction.reply('🚫 | Acesso negado')
+        }
+        try {
+            const code = interaction.options.getString('eval');
+            let evaled = eval(code);
+       
+            if (typeof evaled !== "string")
+              evaled = require("util").inspect(evaled);
+            
+            const saida = new EmbedBuilder()
+                .setTitle("Saída")
+                .setDescription(clean(evaled), {code:"xl"})
+                .setColor("#5097A4")
+            await interaction.reply({ embeds: [saida] })
+          } catch (err) {
+            const embed = new EmbedBuilder()
+            .setTitle('Hmmmmmmm, me parece que ocorreu um erro')
+            .setDescription("```js\n"+clean(err)+"\n```")
+            .setColor("#2f3136")
+            await interaction.reply({ embeds:[embed] })
+        }
+    }
+}
