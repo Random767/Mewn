033a78cd168f110bd557c734a660b975a1355968
GRandom <abupilha@gmail.com>
2023-02-17 20:24:56 -0300

    developer-only commands have been added

--- handling.js
+++ handling.js
@@ -1,9 +1,11 @@
 const { Routes, Collection, REST } = require('discord.js');
+const { developerCommands } = require('./config.json')
 const fs = require(`fs`);
 
 module.exports = (client) => {
   client.commands = new Collection()
   const commands = [];
+  const devCommands = []
   const commandFolder = fs.readdirSync(__dirname + `/commands`)
 
   client.guilds.cache.map(x => x.commands.set([]))
@@ -11,10 +13,17 @@ module.exports = (client) => {
   for(const folder of commandFolder){
     const commandFiles = fs.readdirSync(__dirname + `/commands/${folder}`).filter(file => file.endsWith('.js'))
     for (const file of commandFiles){
-      console.log(`Loading ${folder}/${file}`)
-      const command = require(__dirname + `/commands/${folder}/${file}`);
-      commands.push(command.data.toJSON());
-      client.commands.set(command.data.name, command)
+      if(folder != developerCommands.folder){
+        console.log(`Loading ${folder}/${file}`)
+        const command = require(__dirname + `/commands/${folder}/${file}`);
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command)
+      } else {
+        console.log(`Loading dev ${folder}/${file}`)
+        const command = require(__dirname + `/commands/${folder}/${file}`);
+        devCommands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command)
+      }
     }  
   }
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
@@ -24,7 +33,13 @@ module.exports = (client) => {
         Routes.applicationCommands(client.user.id),
         { body: commands },
       );
-      console.log(`[Load] ${commands.length} comandos carregados`);
+      for(let i = 0; i < developerCommands.whitelist.servers.length; i++){
+        await rest.put(
+          Routes.applicationGuildCommands(client.user.id, developerCommands.whitelist.servers[i]),
+          { body: devCommands },
+        );
+      }
+      console.log(`[Load] ${commands.length + devCommands.length} comandos carregados`);
 
     } catch(err){
       console.error(err);
