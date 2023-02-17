b628429263b4355645ffcae041edc082b9e72777
GRandom <abupilha@gmail.com>
2023-02-17 19:52:46 -0300

    a small change in the embed title

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -9,7 +9,7 @@ module.exports = {
     async execute(interaction, client){
         const response = new EmbedBuilder()
             .setTitle('Central de ajuda')
-            .setDescription(`Olá ${interaction.user.username}! Eu sou o ${client.user.username}, um bot em fase beta, mas futuramente vai vir cheio de functionalidades`)
+            .setDescription(`Olá ${interaction.user.username}! Eu sou o ${client.user.username}, um bot cheio de functionalidades para o seu servidor`)
             .addFields(({ name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: [discord.gg/3WYfg5RV9T](https://discord.gg/3WYfg5RV9T)', inline: true} ))
             .addFields(({ name: '🖥 • Eu sou open source', value: 'Meu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)', inline: true} ))
             .addFields({ name: "🤔 • Quer ver todos os meus comandos?", value: "Utilize o comando /commands", inline: false })
