a5a21814f5f047db8eff8a70653c070f97813fcb
GRandom <abupilha@gmail.com>
2023-02-19 11:37:04 -0300

    field with the name "Quer ver todos os meus  [...]" was removed

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -12,7 +12,6 @@ module.exports = {
             .setDescription(`Olá ${interaction.user.username}! Eu sou o ${client.user.username}, um bot cheio de functionalidades para o seu servidor`)
             .addFields(({ name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: [discord.gg/3WYfg5RV9T](https://discord.gg/3WYfg5RV9T)', inline: true} ))
             .addFields(({ name: '🖥 • Eu sou open source', value: 'Meu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)', inline: true} ))
-            .addFields({ name: "🤔 • Quer ver todos os meus comandos?", value: "Utilize o comando /commands", inline: false })
         
             .setThumbnail(client.user.displayAvatarURL({ dinamic: true, size: 4096, format: "png" }))
             .setColor('#2f3136')
