eeab0d429d4a79ddbf587749c6b6366f16a74ba2
GRandom <abupilha@gmail.com>
2023-02-28 16:38:25 -0300

    added the argument used in the logger and code organization

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -15,12 +15,17 @@ module.exports = {
           console.error(`Nenhum comando com o nome ${interaction.commandName} foi encontrado`);
           return;
         }
+        const options = interaction.options._hoistedOptions
         const log = new EmbedBuilder()
           .setTitle("Alguém executou um comando")
-          .addFields({ name: "Autor", value: "```" + interaction.user.tag + " (" + interaction.user.id + ") ```", inline: false})
-          .addFields({ name: "Servidor", value: "```" + interaction.guild.name + " (" + interaction.guild.id + ")```", inline: false})
-          .addFields({ name: "Canal", value: "```" + interaction.channel.name + " (" + interaction.channel.id + ")```", inline: false})
-          .addFields({ name: "Comando", value: "```" + interaction.commandName + "```", inline: false})
+          .addFields(
+            { name: "Autor", value: "```" + interaction.user.tag + " (" + interaction.user.id + ") ```", inline: false},
+            { name: "Servidor", value: "```" + interaction.guild.name + " (" + interaction.guild.id + ")```", inline: false},
+            { name: "Canal", value: "```" + interaction.channel.name + " (" + interaction.channel.id + ")```", inline: false},
+            { name: "Comando", value: "```" + interaction.commandName + "```", inline: false},
+            { name: "Argumentos", value: `\`\`\`${options[0]?.value ?? 'Nenhum argumento foi utilizado'}\`\`\`` }
+          )
+          .addFields()
           .setThumbnail(interaction.user.displayAvatarURL({dynamic: true}))
           .setColor('#2f3136')
         if(eventLog.isEnabled){
