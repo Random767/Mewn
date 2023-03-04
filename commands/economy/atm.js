3c05e27bb67bcb64982950f0dd2a6fd188bab4f3
GRandom <abupilha@gmail.com>
2023-03-04 16:47:24 -0300

    added atm command

---
+++ commands/economy/atm.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('atm')
+        .setDescription('[Economy] Veja quantos MewnCoins um usuário tem')
+        .addUserOption(option => 
+            option
+                .setName('usuário')
+                .setDescription('Quer ver a quantidade de MewnCoins de qual usuário?')
+        ),
+    async execute(interaction){
+        const JSONdb = require('simple-json-db')
+        const db = new JSONdb('./storage.json')
+        
+        const user = interaction.options.getUser('usuário') || interaction.user
+
+        if(!db.has(user.id)){
+            db.set(user.id, {"name": user.username, "discriminator": user.discriminator, "ld": null, "coins": 0})
+            console.log('Novo usuário criado no banco de dados')
+        }
+
+        const atm = db.get(user.id)
+        if(user === interaction.user){
+            return await interaction.reply(`Você tem ${atm.coins} MewnCoins :D`)
+        }
+        await interaction.reply(`${atm.name} tem ${atm.coins} MewnCoins :D`)
+    }
+}
