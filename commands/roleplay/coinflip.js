3d6711218e07603dc6b473bfa6b86b6158316aae
GRandom <abupilha@gmail.com>
2023-03-05 14:25:17 -0300

    bug fixing

--- commands/roleplay/coinflip.js
+++ commands/roleplay/coinflip.js
@@ -6,15 +6,11 @@ module.exports = {
         .setDescription('[RP] Cara ou coroa?')
         .setDMPermission(false),
     async execute(interaction){
-        if(interaction.options.getSubcommand() == 'bet'){
-            return await interaction.reply('Em breve')
-        } else if(interaction.options.getSubcommand() == 'flip') {
-            const coinflip = [
-                'Cara',
-                'Coroa'
-            ]
-            const result = Math.floor((Math.random() * coinflip.length))
-            await interaction.reply(`:coin: | ${coinflip[result]}!`)
-        }
+        const coinflip = [
+            'Cara',
+            'Coroa'
+        ]
+        const result = Math.floor((Math.random() * coinflip.length))
+        await interaction.reply(`:coin: | ${coinflip[result]}!`)
     }
 }
