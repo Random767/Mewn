5b1cc31e65da7176d6eace517c4805740d915f49
GRandom <abupilha@gmail.com>
2023-03-07 15:00:31 -0300

    adding emojis

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -16,6 +16,7 @@ module.exports = {
                 .setName('quantidade')
                 .setDescription('Quantos MewnCoins você deseja enviar?')
                 .setRequired(true)
+                .setMinValue(1)
         ),
     async execute(interaction){
         const db = new JSONdb(`${__dirname}/../../storage.json`)
@@ -28,15 +29,13 @@ module.exports = {
         }
 
         if(quantity > db.get(interaction.user.id).coins){
-            return await interaction.reply(`Você não tem MewnCoins o suficiente para fazer isso :(`)
-        } else if(quantity < 0){
-            return interaction.reply(`O Número de MewnCoins não pode ser menos que 0`)
+            return await interaction.reply(`:chart_with_downwards_trend: | Você não tem MewnCoins o suficiente!`)
         } else if(interaction.user.id == user.id){
-            return interaction.reply(`Você não pode enviar MewnCoins pra você mesmo :v`)
+            return interaction.reply(`:octagonal_sign: | Você não pode enviar MewnCoins pra você mesmo :v`)
         }
 
         db.set(interaction.user.id, {"name": interaction.user.username, "discriminator": interaction.user.discriminator, "ld": db.get(interaction.user.id).ld, "coins": db.get(interaction.user.id).coins - quantity})
         db.set(user.id, {"name": user.username, "discriminator": user.discriminator, "ld": db.get(user.id).ld, "coins": db.get(user.id).coins + quantity})
-        await interaction.reply(`${quantity} MewnCoins tranferidos para ${user.username} com sucesso!`)
+        await interaction.reply(`:money_with_wings: | **${quantity} MewnCoins** tranferidos para **${user.tag}** com sucesso!`)
     }
 }
