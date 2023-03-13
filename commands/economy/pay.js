93e76c8042e0ce3a313e0878d6c0e70b1ab2a9f1
GRandom <abupilha@gmail.com>
2023-03-13 14:39:16 -0300

    bug fixed and database changed

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -1,10 +1,13 @@
-const { SlashCommandBuilder } = require('discord.js')
-const JSONdb = require('simple-json-db')
+const { SlashCommandBuilder, User } = require('discord.js')
+const SimplDB = require('simpl.db')
+const db = new SimplDB()
+const Users = db.createCollection('users')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('pay')
         .setDescription('[Economy] Transfira MewnCoins pra alguém')
+        .setDMPermission(false)
         .addUserOption((option) => 
             option
                 .setName('usuário')
@@ -19,23 +22,36 @@ module.exports = {
                 .setMinValue(1)
         ),
     async execute(interaction){
-        const db = new JSONdb(`${__dirname}/../../storage.json`)
-
         const user = interaction.options.getUser('usuário')
         const quantity = interaction.options.getNumber('quantidade')
 
-        if(!db.has(user.id)){
-            db.set(user.id, {"name": user.username, "discriminator": user.discriminator, "ld": null, "coins": 0})
+        if(!Users.has(u => u.id == interaction.user.id)){
+            return interaction.reply(':chart_with_downwards_trend: | Você não tem MewnCoins!')
+        }
+        if(!Users.has(u => u.id == user.id)){
+            await Users.create({"id": user.id, "name": user.username, "discriminator": user.discriminator, "ld": null, "coins": 0, aboutme: null, reps: 0})
         }
 
-        if(quantity > db.get(interaction.user.id).coins){
+        if(quantity > Users.get(u => u.id = interaction.user.id).coins){
             return await interaction.reply(`:chart_with_downwards_trend: | Você não tem MewnCoins o suficiente!`)
         } else if(interaction.user.id == user.id){
             return interaction.reply(`:octagonal_sign: | Você não pode enviar MewnCoins pra você mesmo :v`)
         }
 
-        db.set(interaction.user.id, {"name": interaction.user.username, "discriminator": interaction.user.discriminator, "ld": db.get(interaction.user.id).ld, "coins": db.get(interaction.user.id).coins - quantity})
-        db.set(user.id, {"name": user.username, "discriminator": user.discriminator, "ld": db.get(user.id).ld, "coins": db.get(user.id).coins + quantity})
+        Users.update(
+            person => {
+                if(person.id !== interaction.user.id) return 
+                person.coins -= quantity
+            }
+        
+        )
+        Users.update(
+            person => {
+                if(person.id !== user.id) return 
+                person.coins += quantity
+            }
+        )
+
         await interaction.reply(`:money_with_wings: | **${quantity} MewnCoins** tranferidos para **${user.tag}** com sucesso!`)
     }
 }
