56eecfee63ca4acdf3a7706aa956c373bd5ec915
GRandom <abupilha@gmail.com>
2023-03-17 22:51:00 -0300

    re-added a field "Meus comandos"

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -10,8 +10,11 @@ module.exports = {
         const response = new EmbedBuilder()
             .setTitle('Central de ajuda')
             .setDescription(`Olá ${interaction.user.username}! Eu sou o ${client.user.username}, um bot cheio de functionalidades para o seu servidor`)
-            .addFields(({ name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: [discord.gg/3WYfg5RV9T](https://discord.gg/3WYfg5RV9T)', inline: true} ))
-            .addFields(({ name: '🖥 • Eu sou open source', value: 'Meu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)', inline: true} ))
+            .addFields(
+                { name: '❓ • Entre no meu servidor de suporte', value: 'Clicando aqui: [discord.gg/3WYfg5RV9T](https://discord.gg/3WYfg5RV9T)', inline: true},
+                { name: '🖥 • Eu sou open source', value: 'Meu repositório no github: [github.com/Random767/Mewn](https://github.com/Random767/Mewn)', inline: true},
+                { name: '⚙️ • Meus comandos', value: 'Utilize /commands para ver meus comandos!', inline: true}
+            )
         
             .setThumbnail(client.user.displayAvatarURL({ dinamic: true, size: 4096, format: "png" }))
             .setColor('#2f3136')
