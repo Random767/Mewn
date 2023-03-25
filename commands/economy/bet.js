743af750b85420f039a17433dbdded5c9be2faab
GRandom <abupilha@gmail.com>
2023-03-24 23:30:55 -0300

    bug fix

--- commands/economy/bet.js
+++ commands/economy/bet.js
@@ -42,7 +42,7 @@ module.exports = {
         }
 
         if (number > author_info.coins) return await interaction.reply(`Você não pode apostar mais do que você tem :v`)
-        if (number > target_info.coins ?? 0) return await interaction.reply(`O usuário ${user.username} não tem MewnCoins o suficiente!`)
+        if (number > target_info.coins) return await interaction.reply(`O usuário ${user.username} não tem MewnCoins o suficiente!`)
 
         await interaction.reply({ content: `<@${user.id}>, <@${interaction.user.id}> quer fazer uma aposta de ${number} MewnCoins com você.`, ephemeral: false })
 
@@ -84,8 +84,7 @@ module.exports = {
                     content: `:face_with_monocle: | Só <@${user.id}> pode clicar nesse botão!`,
                     ephemeral: true
                 })
-            }
-            if (i.customId === `bet-accept-${message.id}`) {
+            } else if (i.customId === `bet-accept-${message.id}`) {
                 interaction.editReply({ components: [buttons_disabled] })
                 const winner = Math.floor((Math.random() * 2))
                 let users = [
@@ -103,6 +102,7 @@ module.exports = {
                         if (person.id === users[winner]) person.coins += number
                     }
                 )
+                return
             } else if (i.customId === `bet-refuse-${message.id}`) {
                 interaction.editReply({ components: [buttons_disabled] })
                 return await i.reply({
