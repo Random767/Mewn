2e879debd364430e4f06671fc9eb8e753f6d6121
random767 <abupilha@gmail.com>
2023-11-01 13:45:40 -0300

    Number of hours the user needs to get the daily again increased to 24 hours, small change to the message when receiving the daily and bug fixed

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -23,7 +23,7 @@ module.exports = {
         const convert = moment_timezone(Users.get(u => u.id === interaction.user.id).ld).tz('America/Sao_Paulo');
         const hours = moment_timezone().diff(convert, 'hours');
 
-        if(hours < 12 && hours != null){
+        if(hours < 24 && hours != null){
             return await interaction.reply(`:clock12: | Você já pegou seu daily, espere **${12 - hours} horas**!`)
         }
 
@@ -31,8 +31,8 @@ module.exports = {
         Users.update(
             person => {
                 if(person.id === interaction.user.id){
-                    person.coins += daily
                     person.ld = moment().format()
+                    person.coins += daily
                     if(person.name !== interaction.user.username){
                         person.name = interaction.user.username
                         person.discriminator = interaction.user.discriminator
@@ -44,6 +44,6 @@ module.exports = {
         let ranking = fetch.sort((a, b) => b.coins - a.coins)
         let result = ranking.findIndex(usuario => usuario.id === user.id) + 1
 
-        await interaction.reply(`:moneybag: | Você _ganhou_ **${daily} MewnCoins**, agora você tem *${Users.get(u => u.id === interaction.user.id).coins}* MewnCoins e está em **_#${result}_ no rank global de MewnCoins!**!`)
+        await interaction.reply(`:moneybag: | Você _ganhou_ **${daily} MewnCoins**, agora você tem *${Users.get(u => u.id === interaction.user.id).coins}* MewnCoins e está em **_${result}° lugar_ no rank global de MewnCoins!**!`)
     }
 }
