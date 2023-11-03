073a084ac66112c2a4d2bec31e298a575ca7c80c
random767 <abupilha@gmail.com>
2023-11-03 16:46:28 -0300

    daily command renamed to "daily rescue", option to change notification settings and command performance improved

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,9 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js')
-const SimplDB = require('simpl.db')
-const db = new SimplDB({
-    collectionsFolder: __dirname + '/../../collections'
-})
-const Users = db.createCollection('users')
+const Mewn = require("../../index")
+const Users = Mewn.Users
 const moment_timezone = require('moment-timezone')
 const moment = require('moment')
 moment.locale('pt-BR')
@@ -12,13 +9,62 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('daily')
         .setDescription('[Economy] Receba seus MewnCoins diários')
-        .setDMPermission(false),
+        .setDMPermission(false)
+        .addSubcommand(subcommand => subcommand
+          .setName("resgatar")
+          .setDescription("[Economy] Receba seus MewnCoins diários")
+        )
+        .addSubcommandGroup((group) => group
+          .setName("notificações")
+          .setDescription("Não tem descrição")
+            .addSubcommand(subcommand => subcommand
+              .setName("configurar")
+              .setDescription("Configure suas notificações do daily!")
+              .addStringOption(option => option.setName("canal")
+                .setDescription("Onde você quer que a notificação de pegar o daily venha?")
+                .addChoices(
+                  {"name": "Na minha DM", "value": "dm"},
+                  {"name": "Ultimo canal que peguei o daily", "value": "lastChannel"}
+                ).setRequired(false))
+              .addStringOption(option => option.setName("ativado")
+                .setDescription("Quer ativar ou desativar as notificações?")
+                .addChoices(
+                  {"name": "Sim", "value": "true"},
+                  {"name": "Não", "value": "false"}
+                )
+              )
+            )
+        ),
     async execute(interaction) {
-        const user = interaction.user
-        let userinfo = Users.fetch(u => u.id == user.id)
+      const user = interaction.user
+      const userHas = Users.has(u => u.id == user.id)
+      let userinfo = Users.get(u => u.id == user.id)
+
+      switch(interaction.options.getSubcommand()){
+        case "configurar":
+          const channel = interaction.options.getString("canal")
+          const active = interaction.options.getString("ativado")
+          if(!userHas) {
+            return interaction.reply(":octagonal_sign: | Para fazer essa configuração, você precisa **pegar o daily** primeiro utilizando o comando /daily")
+          }
+
+          await interaction.deferReply()
+
+          Users.update(person => {
+            if(person.id == user.id) {
+              if(channel) {
+                person.notifications.daily.preference = channel
+              }
+              if(active) {
+                person.notifications.daily.actived = active
+              }
+            }
+          })
+          return interaction.editReply(`:white_check_mark: | Sua configuração de notificação foi **salva com sucesso**!`)
         
+      case "resgatar":
         if(!Users.has(u => u.id === interaction.user.id)){
-          const userJSON =  {"id": user.id, "name": user.username, "discriminator": user.discriminator, "ld": null, "notifications": {"daily": {"date":null}}, "coins": 0, aboutme: null, reps: 0, banned: false} 
+          const userJSON =  {"id": user.id, "name": user.username, "discriminator": user.discriminator} 
           Users.create(userJSON)
           userinfo = userJSON
         }
@@ -37,6 +83,7 @@ module.exports = {
                     person.ld = moment().format()
                     person.coins = userinfo.coins + daily
                     person.notifications.daily.date = null
+                    person.notifications.daily.channelId = interaction.channel.id
                     if(person.name !== interaction.user.username){
                         person.name = interaction.user.username
                         person.discriminator = interaction.user.discriminator
@@ -44,10 +91,11 @@ module.exports = {
                 }
             }
         )
-        const fetch = Users.fetchAll()
-        let ranking = fetch.sort((a, b) => b.coins - a.coins)
+        const usersGet = Users.getAll()
+        let ranking = usersGet.sort((a, b) => b.coins - a.coins)
         let result = ranking.findIndex(usuario => usuario.id === user.id) + 1
 
         await interaction.reply(`:moneybag: | Você _ganhou_ **${daily} MewnCoins**, agora você tem *${Users.get(u => u.id === interaction.user.id).coins}* MewnCoins e está em **_${result}° lugar_ no rank global de MewnCoins!**!`)
     }
+  }
 }
