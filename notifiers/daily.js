13b46afb6a5b2450f9a6358ecdd32f72978fd4c5
random767 <abupilha@gmail.com>
2023-11-03 22:06:36 -0300

    Improved code structure, making it easier for people to maintain and interpret the code in the future. Fixed problem with the channel variable being created too early

--- notifiers/daily.js
+++ notifiers/daily.js
@@ -7,10 +7,22 @@ const Users = Mewn.Users
 
 module.exports = (client) => {
   cron.schedule("* * * * *", () => {
-    Users.fetchAll().forEach(user => {
+    Users.getAll().forEach(user => {
       const convert = moment_timezone(user.ld).tz('America/Sao_Paulo');
       const hours = moment_timezone().diff(convert, 'hours');
       try {
+        if(user.notifications.daily.active == "false") {
+          return
+        } else if (!user.notifications.daily.channelId){
+          return
+        } else if(user.notifications.daily.date){
+          return
+        } else if(user.ld == null) {
+          return
+        } else if(hours < 24) {
+          return
+        }
+
         let channel;
         switch(user.notifications.daily.preference){
           case "dm":
@@ -18,28 +30,22 @@ module.exports = (client) => {
             break
           case "lastChannel":
             channel = client.channels.cache.get(user.notifications.daily.channelId)
-          break
+            break
         }
-        if(user.notifications.daily.date){
-          return
-        } else if(user.notifications.daily.active == "false") {
-          return
-        }
-        if(user.ld != null && hours >= 24){
-          const dailyMsg = new EmbedBuilder()
-            .setTitle("Pegue seu daily agora")
-            .setDescription("As 24 horas já se passaram e você já pode pegar seu daily novamente :D Utilize o comando /daily resgatar")
-            .setThumbnail(client.user.avatarURL({ dynamic: true, size: 4096, format: "png" }))
-            .setColor("#40bf40")
-          channel.send({ content: `<@${user.id}>`, embeds: [dailyMsg] })
-          Users.update(
-            person => {
-              if(person.id == user.id) {
-                person.notifications.daily.date = moment().format()
-              }
+
+        const dailyMsg = new EmbedBuilder()
+          .setTitle("Pegue seu daily agora")
+          .setDescription("As 24 horas já se passaram e você já pode pegar seu daily novamente :D Utilize o comando /daily resgatar")
+          .setThumbnail(client.user.avatarURL({ dynamic: true, size: 4096, format: "png" }))
+          .setColor("#40bf40")
+        channel.send({ content: `<@${user.id}>`, embeds: [dailyMsg] })
+        Users.update(
+          person => {
+            if(person.id == user.id) {
+              person.notifications.daily.date = moment().format()
             }
-          ) 
-        }
+          }
+        )
       } catch(err) {
         console.error(err)
       }
