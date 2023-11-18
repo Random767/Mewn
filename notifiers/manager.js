bb4ff664b20aaf9763f7c5d973b481815183ad1f
random767 <abupilha@gmail.com>
2023-11-18 10:39:21 -0300

    notification manager added

---
+++ notifiers/manager.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const cron = require('node-cron')
+const Mewn = require('../index')
+const Users = Mewn.Users
+const archives = fs.readdirSync(__dirname + '/push')
+
+function scheduleNotifications(){
+  try {
+    const allUsers = Users.getAll()
+    archives.forEach(archive => {
+      const push = require(`${__dirname}/push/${archive}`)
+      const infos = push.getData(allUsers)
+      infos.forEach(user => {
+        let channel;
+        switch(user.channel.type) {
+          case "lastChannel":
+            channel = Mewn.client.channels.cache.get(user.channel.id)
+            break
+          case "dm":
+            channel = Mewn.client.users.cache.get(user.channel.id)
+            break
+        }
+
+        push.updateDate(user.id, user.dateId != undefined ? user.dateId : null)
+
+        if(user.date === "now"){
+          channel.send(user.message)
+          return
+        }
+        cron.schedule(user.date, () => {
+          channel.send(user.message)
+        })
+      }); 
+    
+    });
+  } catch(err){
+    console.error(err)
+  }
+}
+
+scheduleNotifications()
+
+setInterval(() => {
+  scheduleNotifications()
+}, 86400000)
