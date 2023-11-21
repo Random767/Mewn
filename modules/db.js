4797b1e2374f32f111e88d95cf5a305aadeb1ffd
random767 <abupilha@gmail.com>
2023-11-21 20:40:41 -0300

    basic database module added

---
+++ modules/db.js
@@ -0,0 +1,50 @@
+const SimplDB = require('simpl.db')
+const db = new SimplDB({
+  collectionsFolder: `${__dirname}/../collections`
+})
+const usersDefaultFormat = require('./../presets/db/users.json')
+const Users = db.createCollection('users', usersDefaultFormat)
+
+function operations(){
+  function methods(data){
+    function create(operation){
+      const result = data.create(operation)
+      return result
+    }
+    function get(operation){
+      const result = data.get(operation)
+      return result
+    }
+    function has(operation){
+      const result = data.has(operation)
+      return result
+    }
+    function update(operation){
+      const result = data.update(operation)
+      return result
+    }
+    function getAll(operation){
+      const result = data.getAll(operation)
+      return result
+    }
+
+    return {
+      create: create,
+      get: get,
+      has: has,
+      update: update,
+      getAll: getAll
+    }
+  }
+
+  function users(){
+    return methods(Users)
+  }
+  return {
+    users: users()
+  }
+}
+
+operations().users.create({ id: 0, name: "gr" })
+
+module.exports = operations()
