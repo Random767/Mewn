549b3f997c68318e9fa57a25d43a590a792853a2
GRandom <RandG0@proton.me>
2025-08-27 05:11:00 -0300

    Agora os logs ficam na pasta ./logs

--- modules/logger.js
+++ modules/logger.js
@@ -1,16 +1,24 @@
 const fs = require('fs/promises')
+const { mkdirSync } = require('fs')
 const moment = require('moment')
 const config = require(`${__dirname}/../config.json`)
 moment.locale('pt-BR')
 
 const infos = {
-  file: `${__dirname}/../${config.eventLog.file.filename}`,
+  file: config.eventLog.file.filename,
+  dir: `${__dirname}/../logs/`,
   overWrite: false
+}
+
+try{
+  mkdirSync(infos.dir)
+} catch(err) {
+  if(err.code !== 'EEXIST') throw err;
 }
 
 async function write(content) {
   try {
-    await fs.appendFile(infos.file, content);
+    await fs.appendFile(`${infos.dir}${infos.file}`, content);
   } catch (err) {
     console.log(err);
   }
@@ -18,7 +26,7 @@ async function write(content) {
 
 async function debugWrite(content) {
   try {
-    await fs.appendFile('debug.log', content);
+    await fs.appendFile('logs/debug.log', content);
   } catch (err) {
     console.log(err);
   }
