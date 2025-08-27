327c976f92832934ce2740c557dd71874b6c73a1
GRandom <RandG0@proton.me>
2025-08-27 05:21:07 -0300

    Teste básico do módulo de transação será mantido, por enquanto

--- tests/transaction.js
+++ tests/transaction.js
@@ -5,8 +5,8 @@ const atm = require('./../commands/economy/atm')
 
 // template de como deve funcionar o módulo de transação
 const dateNow = Date.now()
-//transaction.make("daily", {"reciver_id": 633764019559202836, "amount": 8502, "timestamp": dateNow, "messaage": ""});
-//transaction.make("pay", {"sender_id" : 4, "reciver_id": 0, "amount": 0, "timesamp": 0, "messaage": ""})
+transaction.make("index", {"reciver_id": 1, "amount": 1, "timestamp": dateNow, "messaage": ""});
+//transaction.make("pay", {"sender_id" : 1, "reciver_id": 633764019559202836, "amount": -1000, "timestamp": dateNow, "messaage": ""})
 
 // Testes de transações para os comandos de economia
 // Interaction é o template simulando um usuário
@@ -39,6 +39,6 @@ const Interaction = {
 
 
 // Saídas devem ser checadas no terminal
-daily.execute(Interaction)
-atm.execute(Interaction)
-pay.execute(Interaction)
+//daily.execute(Interaction)
+//atm.execute(Interaction)
+//pay.execute(Interaction)
